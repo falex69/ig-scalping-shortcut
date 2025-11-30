88e702cb44aabf9286f5509d8aebfda5f4013990
falex69 <falex@free.fr>
2025-11-30 12:26:44 +0100

    Update oneclick-shortcut.js

--- oneclick-shortcut.js
+++ oneclick-shortcut.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         IG Buy/Sell OneClick Shortcut
 // @namespace    http://tampermonkey.net/
-// @version      2025-03-13
+// @version      2025-07-15
 // @description  try to take over the world!
-// @author       falex
+// @author       falex69
 // @match        https://demo-deal.ig.com/web-platform/
 // @match        https://deal.ig.com/web-platform/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ig.com
@@ -16,22 +16,19 @@ function doc_keyUp(e) {
         case 130:
         case 133:
             //F19 & Screen(HP) -> Buy Open ticket
-            //document.getElementsByClassName("ig-ticket-oneclick_price cell-price btn ig-price-button ig-price-button--buy ig-price-button--active ember-view")[0].click()
-            document.getElementsByClassName("cell-price btn ig-price-button ig-price-button--buy ig-price-button--active ig-ticket-oneclick_price")[0].click();
+            document.getElementsByClassName("ig-ticket-oneclick_price cell-price btn ig-price-button ig-price-button--buy ig-price-button--active")[0].click()
             break;
          case 129:
          case 124:
             //F18 & F12(HP) -> Sell Open ticket
-            // document.getElementsByClassName("ig-ticket-oneclick_price cell-price btn ig-price-button ig-price-button--sell ig-price-button--active ember-view")[0].click()
-            document.getElementsByClassName("cell-price btn ig-price-button ig-price-button--sell ig-price-button--active ig-ticket-oneclick_price")[0].click();
+            document.getElementsByClassName("ig-ticket-oneclick_price cell-price btn ig-price-button ig-price-button--sell ig-price-button--active")[0].click()
             break;
         case 19:
         case 113:
         case 127:
-            //F16 (Win10) & F2(HP) & F16 (MacOS) -> Close ticket
+            //F16 & F2(HP) -> Close ticket
             var list = document.getElementsByClassName("btn btn-grid cell-close_btn btn-submit");
             for (var i = list.length -1 ; i >= 0; i--) {
-                //console.log("i="+i)
                 document.getElementsByClassName("btn btn-grid cell-close_btn btn-submit")[i].click();
             }
             break;
@@ -39,10 +36,7 @@ function doc_keyUp(e) {
         case 121:
             //F17 & F10(HP) -> Next Tab
             var el = document.getElementsByClassName("ig-market-view-title ember-view"); //HTMLCollection of buttons
-            console.log(el[0].parentNode.parentNode.className);
             for (var j = 0; j<el.length; j++) {
-                //Update 20250107 no more ember-view
-                //if (el[j].parentNode.parentNode.className == "workspace-panel_tab-title workspace-panel_tab-title--active ember-view") {
                 if (el[j].parentNode.parentNode.className == "workspace-panel_tab-title workspace-panel_tab-title--active") {
                     console.log(j+" est l'onglet sélectionné");
                     var nextitem = (1+j)%el.length; //next item using modulus operator
@@ -57,4 +51,3 @@ function doc_keyUp(e) {
 }
 
 document.addEventListener('keyup', doc_keyUp, false);
-
